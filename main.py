4143587483417cdb7e4fc416ee765d40c15b62eb
rafaelanjos <157972262+RafaAnjos25@users.noreply.github.com>
2025-06-18 21:00:58 -0300

    rota registrar

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 from flask import Flask, render_template, request, jsonify
 from db import db
 from models import Usuario
+import os
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dados.db"
 db.init_app(app)
 
-@app.route('/registrar')
+@app.route('/registrar', methods=["Post"])
 def registrar():
     data = request.get_json()
 
     if not data:
-        return jsonify({"error": "Nenhum dado enviado"}), 400
+        os.abort("Error: Nenhum dado enviado"), 400
     
     campos_requeridos = ["nome", "email", "senha"]
+    for campos in campos_requeridos:
+        if campos not in data:
+            return jsonify({"error": f"Campo obrigatório ausente: {campos}"}), 400
 
+    try:         
+        novo_usuario = Usuario(
+            nome=data["nome"], 
+            email=data["email"],
+            senha=data["senha"])
+        db.session.add(novo_usuario)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso"}), 201
+    except Exception as e:
+        return jsonify({"error: str(e)"}), 400
+
+#@app.route('/deletar', methods=["Delete"])
 
 if __name__ == '__main__':
     with app.app_context():
--- models.py
+++ models.py
@@ -6,7 +6,6 @@ class Usuario(db.Model):
     nome = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(40), nullable=False, unique=True)
     senha = db.Column(db.String(20), nullable=False)
-    time = db.Column(db.String(20))
 
     def __repr__(self):
         return f"<{self.nome}"
